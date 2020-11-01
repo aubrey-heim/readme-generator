1fd3ea89503966585a26a4b527e159ea3f002c08
aubrey-heim <aubrey.l.heim@gmail.com>
2020-11-01 09:25:42 -0800

    created README skeleton

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var fs = require("fs");
-var inquirer = require("inquirer");
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 inquirer
 .prompt([
@@ -18,8 +18,38 @@ inquirer
         const {} = response
 
 
-        let page = ``
+        let page = `# TITLE
 
+        ## Table of Contents
+        * [Description](#description)
+            * [Deployed Application](#deployed-application)
+        * [Benefits of This Project](#benefits-of-this-project)
+        * [Installation](#installation)
+        * [Usage](#usage)
+        * [License](#license)
+        * [Contributing](#contributing)
+        * [Tests](#test)
+        * [Questions](#questions)
+          
+        ## Description
+        
+        ### Deployed Application
+        <img src="FILE PATH" alt="IMAGE/GIF/VIDEO of deployed page in use">
+
+        ## Installation
+
+        ## Usage
+
+        ## License        
+        
+        ## Contributing
+
+        ## Tests
+
+        ## Questions
+       
+        `
+        
     fs.writeFile("README.md", page, function(err) {
 
     if (err) {
